5e9fb51f0bfe3010d63dd405e47c2f3768f2d529
Arpith K <arpith@live.com>
2015-10-21 23:22:52 +0530

    Implemented required data structures and accessor and mutator functions

--- workspace/PAV15Project/src/PAVpointerAnalysisPackage/PAVPointerAnalysis.java
+++ workspace/PAV15Project/src/PAVpointerAnalysisPackage/PAVPointerAnalysis.java
@@ -1,6 +1,5 @@
 package PAVpointerAnalysisPackage;
 
-
 /**
  * @author Suvam Mukherjee
  * Program Analysis and Verification, 2013
@@ -13,6 +12,7 @@ import java.util.*;
 
 import PAVpointerAnalysisPackage.PAVPointerAnalysis;
 import PAVpointerAnalysisPackage.SetUpAnalysis;
+import PAVpointerAnalysisPackage.NullDereference;
 
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IField;
@@ -39,74 +39,78 @@ import com.ibm.wala.util.config.AnalysisScopeReader;
 import com.ibm.wala.util.io.FileProvider;
 import com.ibm.wala.ssa.analysis.ExplodedControlFlowGraph;
 
-
 public class PAVPointerAnalysis {
 
-	private SetUpAnalysis setup;	// Object to setup (check presentation)
-	
-	public PAVPointerAnalysis(String classpath, String mainClass, String analysisClass, String analysisMethod)
-	{
+	private SetUpAnalysis setup; // Object to setup (check presentation)
+	public NullDereference nulldereference;
+
+	public PAVPointerAnalysis(String classpath, String mainClass, String analysisClass, String analysisMethod) {
 		setup = new SetUpAnalysis(classpath, mainClass, analysisClass, analysisMethod);
 	}
-	
-	public void runAnalysis() throws Exception
-	{
+
+	public void runAnalysis() throws Exception {
 		/**
-		 * Use setup to construct the relevant data structures (cfg, etc).
-		 * and to perform analysis
-		 * Print out analysis results to file, in the format specified.
+		 * Use setup to construct the relevant data structures (cfg, etc). and
+		 * to perform analysis Print out analysis results to file, in the format
+		 * specified.
 		 */
-		
-		//START: NO CHANGE REGION
+
+		// START: NO CHANGE REGION
 		setup.buildScope();
 		setup.buildClassHierarchy();
 		setup.buildEntryPoints();
 		setup.setUpCallGraphConstruction();
 		setup.generateCallGraph();
-		//END: NO CHANGE REGION
-		
+		// END: NO CHANGE REGION
+
 		/*
-		 * For demonstration purposes, and to help you check whether Wala is setup properly, I am calling the setup.printNodes() function.
-		 * This will compute the call graph for the given program, and print out the nodes. Feel free to erase this call, or use it otherwise. 
+		 * For demonstration purposes, and to help you check whether Wala is
+		 * setup properly, I am calling the setup.printNodes() function. This
+		 * will compute the call graph for the given program, and print out the
+		 * nodes. Feel free to erase this call, or use it otherwise.
 		 */
 		setup.printNodes();
 		setup.printIR();
 		
+		nulldereference = new NullDereference();
+		nulldereference.entry();
+
 		/*
-		 * Create appropriate objects/make appropriate function calls here to begin the analysis
+		 * Create appropriate objects/make appropriate function calls here to
+		 * begin the analysis
 		 */
 	}
-	
+
 	// START: NO CHANGE REGION
 	/**
-	 * The main method reads in the arguments from the command line and sets up the appropriate variables.
+	 * The main method reads in the arguments from the command line and sets up
+	 * the appropriate variables.
+	 * 
 	 * @param args
-	 * args[0]: Full path of the application jar file. 
-	 * args[1]: Fully qualified name of the class containing the main method. The name is of the form L<package_name>/<class_name>.
-	 * 			For example, to analyze the class test1 in package tests, the argument would be Ltests/test1
-	 * args[2]: Fully qualified name of the class containing the method which we want to analyze.
-	 * args[3]: Name of the method we want to analyze.
+	 *            args[0]: Full path of the application jar file. args[1]: Fully
+	 *            qualified name of the class containing the main method. The
+	 *            name is of the form L<package_name>/<class_name>. For example,
+	 *            to analyze the class test1 in package tests, the argument
+	 *            would be Ltests/test1 args[2]: Fully qualified name of the
+	 *            class containing the method which we want to analyze. args[3]:
+	 *            Name of the method we want to analyze.
 	 * 
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception
-	{
-		
+	public static void main(String[] args) throws Exception {
+
 		String classpath, mainClass, analysisClass, analysisMethod;
-				
+
 		classpath = args[0];
 		mainClass = args[1];
 		analysisClass = args[2];
 		analysisMethod = args[3];
-		
-		
-		PAVPointerAnalysis pAnalysis = new PAVPointerAnalysis(classpath, mainClass, analysisClass, 
-										analysisMethod);
-		
+
+		PAVPointerAnalysis pAnalysis = new PAVPointerAnalysis(classpath, mainClass, analysisClass, analysisMethod);
+
 		pAnalysis.runAnalysis();
-		
+
 	}
 	// END: NO CHANGE REGION
-
 
 }
